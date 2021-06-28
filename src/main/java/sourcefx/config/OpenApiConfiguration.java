6f4d62e9dd293c5b88aef27b7507d92707eaf556
caidy <vcsdk2468@gmail.com>
2021-06-28 12:25:16 +0800

    引入Knife4j

--- src/main/java/sourcefx/config/OpenApiConfiguration.java
+++ src/main/java/sourcefx/config/OpenApiConfiguration.java
@@ -3,26 +3,31 @@ package sourcefx.config;
 import static org.springdoc.core.SpringDocUtils.getConfig;
 
 import java.util.Arrays;
-import java.util.Objects;
-import java.util.stream.Stream;
 
-import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Pageable;
-import org.springframework.util.AntPathMatcher;
-import org.springframework.util.PathMatcher;
+
+import com.google.common.net.HttpHeaders;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "SourceFX", version = "1.0"))
-@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "accessToken")
+@OpenAPIDefinition(
+		info = @Info(title = "SourceFX"))
+@SecurityScheme(
+		name = "user",
+		type = SecuritySchemeType.HTTP,
+		scheme = "Bearer",
+		paramName = HttpHeaders.AUTHORIZATION,
+		in = SecuritySchemeIn.HEADER,
+		bearerFormat = "accessToken")
 public class OpenApiConfiguration {
 	static {
 		getConfig()
@@ -30,26 +35,15 @@ public class OpenApiConfiguration {
 	}
 
 	@Bean
-	public OpenApiCustomiser openApiCustomiser() {
-		PathMatcher matcher = new AntPathMatcher();
-		return c -> {
-			c.getPaths().forEach((path, item) -> {
-				if (matcher.match("/api/auth/login", path)) {
-					return;
-				}
-				if (matcher.match("/api/**", path)) {
-					Operation[] ops = new Operation[] {
-							item.getGet(), item.getPost(), item.getDelete(), item.getHead(),
-							item.getOptions(), item.getPatch(), item.getPut()
-					};
-					Stream.of(ops)
-							.filter(Objects::nonNull)
-							.forEach(op -> {
-								op.setSecurity(Arrays.asList(
-										new SecurityRequirement().addList("bearerAuth")));
-							});
-				}
-			});
-		};
+	public GroupedOpenApi adminApi() {
+		return GroupedOpenApi.builder()
+				.group("后台管理")
+				.pathsToMatch("/api/**")
+				.addOperationCustomizer((operation, handlerMethod) -> {
+					operation.setSecurity(Arrays.asList(
+							new SecurityRequirement().addList("user")));
+					return operation;
+				})
+				.build();
 	}
 }
